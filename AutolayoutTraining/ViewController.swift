fa930516da274642168304458a2eea3ab9aadfd8
Alex Mefisto <alexandr.mefisto@poly-tech.com.ua>
2023-04-11 12:15:50 +0300

    Initial Commit

---
+++ AutolayoutTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutolayoutTraining
+//
+//  Created by Alexandr Mefisto on 11.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
